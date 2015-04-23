2c19a7b2a3d11488b0cfe226fa6ea1835d50a4e7
hookyboysb <hookyboysb@gmail.com>
2015-04-22 23:44:24 -0400

    Door Fix
    Fixed the doors so they close over ScreenSelectMusic rather than at the
    screen transition. Also adjusted the DDR logo's disappearance on
    ScreenStageInformation. Also adjusted the size of the jacket to match
    the actual game.
    
    To do:
    Stop preview music when the door closes
    DDR logo "flash" when doors open
    Score panel

--- BGAnimations/ScreenStageInformation in/default.lua
+++ BGAnimations/ScreenStageInformation in/default.lua
@@ -30,23 +30,18 @@ t[#t+1] = Def.ActorFrame {
 	};
 	---- DOOR OPEN > CLOSE  CLOSE > OPEN
 		LoadActor("../2013Door01.png")..{
-		InitCommand=cmd(zoomx,1.1;x,SCREEN_CENTER_X;y,-360;);
-		OnCommand=cmd(linear,0.25;y,0;sleep,1.80;linear,0.45;y,-360);
+		InitCommand=cmd(zoomx,1.1;x,SCREEN_CENTER_X;y,0;);
+		OnCommand=cmd(sleep,1.80;linear,0.45;y,-360);
 	};
 		LoadActor("../2013Door02.png")..{
-		InitCommand=cmd(zoomx,1.1;x,SCREEN_CENTER_X;y,360;);
-		OnCommand=cmd(linear,0.25;y,0;sleep,1.80;linear,0.45;y,360);
+		InitCommand=cmd(zoomx,1.1;x,SCREEN_CENTER_X;y,0;);
+		OnCommand=cmd(sleep,1.80;linear,0.45;y,360);
 	};
-	-- Door sound
-	LoadActor(THEME:GetPathS( "", "_Door" ) ) .. {
-		StartTransitioningCommand=function(self)
-			self:play();
-		end;
-	};
+
 	--- DDR2013 LOGO  apparition > disparition ---
     LoadActor("../DDR_logo_Doors.png")..{
 		InitCommand=cmd(x,SCREEN_CENTER_X;y,0);
-		OnCommand=cmd(diffusealpha,0;sleep,0.25;linear,0.25;diffusealpha,1;sleep,1.50;linear,0.25;diffusealpha,0);
+		OnCommand=cmd(sleep,1.80;linear,0.25;diffusealpha,0);
 	};
 	--- Flash SONG BANNER  sound------
 	LoadActor("SoundStage") .. {
@@ -58,7 +53,7 @@ t[#t+1] = Def.ActorFrame {
 	};
 };
 t[#t+1] = Def.Quad{
-	OnCommand=cmd(diffuse,color("#000000");Center;setsize,326,326;diffusealpha,0;zoom,4;sleep,1.9;linear,0.2;diffusealpha,1;zoom,1);
+	OnCommand=cmd(diffuse,color("#000000");Center;setsize,464,464;diffusealpha,0;zoom,4;sleep,1.9;linear,0.2;diffusealpha,1;zoom,1);
 	};
 if not GAMESTATE:IsCourseMode() then
 --song jacket--
@@ -69,13 +64,13 @@ t[#t+1] = Def.ActorFrame {
 		local song = GAMESTATE:GetCurrentSong();
 			if song:HasJacket() then
 				self:LoadBackground(song:GetJacketPath());
-				self:setsize(320,320);
+				self:setsize(460,460);
 			elseif song:HasBackground() then
 				self:LoadFromSongBackground(GAMESTATE:GetCurrentSong());
-				self:setsize(320,320);
+				self:setsize(460,460);
 			else
 				self:Load(THEME:GetPathG("","Common fallback jacket"));
-				self:setsize(320,320);
+				self:setsize(460,460);
 			end;
 		end;
 	};
